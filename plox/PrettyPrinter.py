f3df358858b63cd40912e1380c2a0166a652ab2b
mbianchif <89595589+mbianchif@users.noreply.github.com>
2025-09-06 15:16:21 -0300

    arreglo un poco el codigo

--- plox/PrettyPrinter.py
+++ plox/PrettyPrinter.py
@@ -54,7 +54,7 @@ class PrettyPrinter:
         self._stmt_f = stmt_f
         self._type_f = type_f
 
-        # Reseted
+        # Necesita reset
         self._entries: list[tuple[str, str, str, Callable[[str], str]]] = []
 
     # Printear por stdout el ast de forma más legible
@@ -62,16 +62,25 @@ class PrettyPrinter:
         for stmt in stmts:
             self._reset()
             self._accept(stmt)
+            ast = self._build_tree()
+            print(ast, end="\n\n")
 
-            entries, max_length = [], 0
+    def _build_tree(self) -> str:
+        entries = []
+        max_len_branch, max_len_cls_name = 0, 0
 
-            for entry in self._entries:
-                padding, tag, cls_name = self._apply(*entry)
-                max_length = max(max_length, len(padding) + len(tag))
-                entries.append((padding, tag, cls_name))
+        for entry in self._entries:
+            branch, cls_name = self._apply(*entry)
+            max_len_branch = max(max_len_branch, len(branch))
+            max_len_cls_name = max(max_len_cls_name, len(cls_name))
+            entries.append((branch, cls_name))
 
-            s = "\n".join(f"{(p + t).ljust(max_length)}  {c}" for p, t, c in entries)
-            print(s, end="\n\n")
+        ast = "\n".join(
+            f"{c.ljust(max_len_cls_name)}  {b.ljust(max_len_branch)}"
+            for b, c in entries
+        )
+
+        return ast
 
     def _reset(self):
         self._entries.clear()
@@ -97,11 +106,13 @@ class PrettyPrinter:
 
     @_accept.register
     def _(self, stmt: VarDecl):
-        if stmt._initializer is None:
-            return
-
         tag = stmt._name.lexeme
-        self._shifted(tag, stmt, lambda: self._accept(stmt._initializer))
+
+        def shifted():
+            if stmt._initializer:
+                self._accept(stmt._initializer)
+
+        self._shifted(tag, stmt, shifted)
 
     @_accept.register
     def _(self, stmt: FunDecl):
@@ -117,10 +128,13 @@ class PrettyPrinter:
 
     @_accept.register
     def _(self, stmt: ReturnStmt):
-        if stmt._value is None:
-            return
+        tag = "return"
 
-        self._shifted("return", stmt, lambda: self._accept(stmt._value))
+        def shifted():
+            if stmt._value:
+                self._accept(stmt._value)
+
+        self._shifted(tag, stmt, shifted)
 
     @_accept.register
     def _(self, stmt: IfStmt):
@@ -130,21 +144,20 @@ class PrettyPrinter:
         if stmt._elseBranch:
             self._shifted(
                 "else",
-                stmt,
+                stmt._elseBranch,
                 lambda: self._accept(stmt._elseBranch),
             )
 
     @_accept.register
     def _(self, stmt: WhileStmt):
         self._shifted("while", stmt, lambda: self._accept(stmt._condition))
-        self._shifted("do", stmt, lambda: self._accept(stmt._body))
+        self._shifted("do", stmt._body, lambda: self._accept(stmt._body))
 
     # ---------- Printers de Expresiones ---------- #
 
     @_accept.register
     def _(self, expr: BinaryExpr | LogicExpr):
-        tag = expr._operator.lexeme
-        self._format(tag, expr)
+        self._store(expr._operator.lexeme, expr)
         self._branch(Dir.RIGHT, lambda: self._accept(expr._right))
         self._branch(Dir.LEFT, lambda: self._accept(expr._left))
 
@@ -159,53 +172,51 @@ class PrettyPrinter:
         else:
             tag = str(expr._value)
 
-        self._format(tag, expr)
+        self._store(tag, expr)
 
     @_accept.register
     def _(self, expr: UnaryExpr):
-        tag = expr._operator.lexeme
-        self._format(tag, expr)
+        self._store(expr._operator.lexeme, expr)
         self._branch(Dir.LEFT, lambda: self._accept(expr._right))
 
     @_accept.register
     def _(self, expr: CallExpr):
         self._accept(expr._callee)
 
-        # Como el nombre de la función es una expresión hay
-        # cambiarle el tipo a callee de `VariableExpr` a `CallExpr`
+        # Como el nombre de la función es una expresión le
+        # cambio el tipo a callee de `VariableExpr` a `CallExpr`
         padding, tag, _, f = self._entries[-1]
         cls_name = expr.__class__.__name__
         self._entries[-1] = padding, tag, cls_name, f
 
-        def shifted():
-            for arg in reversed(args[1:]):
-                self._accept(arg)
-
         if args := expr._arguments:
+
+            def shifted():
+                for arg in reversed(args[1:]):
+                    self._accept(arg)
+
             self._branch(Dir.RIGHT, shifted)
             self._branch(Dir.LEFT, lambda: self._accept(args[0]))
 
     @_accept.register
     def _(self, expr: VariableExpr):
-        tag = expr._name.lexeme
-        self._format(tag, expr)
+        self._store(expr._name.lexeme, expr)
 
     @_accept.register
     def _(self, expr: AssignmentExpr):
-        tag = expr._name.lexeme
-        self._format(tag, expr)
+        self._store(expr._name.lexeme, expr)
         self._branch(Dir.LEFT, lambda: self._accept(expr._value))
 
     @_accept.register
     def _(self, expr: PostfixExpr):
-        tag = expr._operator.lexeme
-        self._format(tag, expr)
+        self._store(expr._operator.lexeme, expr)
         self._branch(Dir.LEFT, lambda: self._accept(expr._left))
 
     # ---------- Helpers ---------- #
 
-    def _format(self, tag: str, obj: Expr | Stmt):
-        padding = self._branch_f(self._make_padding())
+    # Guarda una entrada en el arreglo
+    def _store(self, tag: str, obj: Expr | Stmt):
+        padding = self._make_padding()
         cls_name = obj.__class__.__name__
 
         if isinstance(obj, Expr):
@@ -219,7 +230,7 @@ class PrettyPrinter:
     # Mueve el subárbol hacia la derecha en una unidad de offset y devuelve
     # la concatenación entre `tag` y el subarbol generado
     def _shifted(self, tag: str, stmt: Stmt, f: Callable[[], None]):
-        self._format(tag, stmt)
+        self._store(tag, stmt)
         self._shift += 1
         f()
         self._shift -= 1
@@ -250,8 +261,10 @@ class PrettyPrinter:
         return " " * (4 * self._shift) + "".join(parts)
 
     # Aplica las funciones provistas en el constructuor a un entry
-    def _apply(self, padding: str, tag: str, cls_name: str, f: Callable[[str], str]):
+    def _apply(
+        self, padding: str, tag: str, cls_name: str, f: Callable[[str], str]
+    ) -> tuple[str, str]:
         padding = self._branch_f(padding)
         tag = f(tag)
         cls_name = self._type_f(cls_name) if self._type_f else f(cls_name)
-        return padding, tag, cls_name
+        return padding + tag, cls_name
